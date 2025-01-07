72d3fad623469b44fad579ebd9850fbbe25a62db
esevasqu <esevasqu@bancolombia.com.co>
2025-01-06 20:39:31 -0500

    Se realiza el cambio de Api Rest a microservicio

--- src/teams/teams.controller.ts
+++ src/teams/teams.controller.ts
@@ -1,40 +1,59 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, Query } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  Query,
+} from '@nestjs/common';
 import { TeamsService } from './teams.service';
 import { CreateTeamDto } from './dto/create-team.dto';
 import { UpdateTeamDto } from './dto/update-team.dto';
 import { PaginationDto } from 'src/common/dto/pagination.dto';
-
+import { MessagePattern, Payload } from '@nestjs/microservices';
 
 @Controller('teams')
 export class TeamsController {
   constructor(private readonly teamsService: TeamsService) {}
 
-  @Post()
-  create(@Body() createTeamDto: CreateTeamDto) {
-    
+  //@Post()
+  @MessagePattern('create-team') //puede usarse un string o un objeto (create-team)
+  create(@Payload() createTeamDto: CreateTeamDto) {
     //return createTeamDto;
     return this.teamsService.create(createTeamDto);
   }
 
-  @Get()
-  findAll(@Query() paginationDto: PaginationDto) {
+  //@Get()
+  @MessagePattern('get-all-teams')
+  findAll(@Payload() paginationDto: PaginationDto) {
     //return paginationDto;
     return this.teamsService.findAll(paginationDto);
   }
 
-  @Get(':id')
-  findOne(@Param('id') id: string) {
+  //@Get(':id')
+  @MessagePattern('find-one-team')
+  findOne(@Payload('id') id: string) {
     //return id;
     return this.teamsService.findOne(id);
   }
 
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateTeamDto: UpdateTeamDto) {
-    return this.teamsService.update(id, updateTeamDto);
+  //@Patch(':id')
+  @MessagePattern('update-one-team')
+  update(
+    //@Param('id') id: string, 
+    //@Body() updateTeamDto: UpdateTeamDto
+    @Payload() updateTeamDto: UpdateTeamDto,
+
+    
+  ) {
+    return this.teamsService.update(updateTeamDto.id, updateTeamDto);
   }
 
-  @Delete(':id')
-  remove(@Param('id') id: string) {
+  //@Delete(':id')
+  @MessagePattern('delete-one-team')
+  remove(@Payload('id') id: string) {
     return this.teamsService.remove(id);
   }
 }
